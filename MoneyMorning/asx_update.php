06ade6fd0321340e6a27de7ebef2aae658b57ded
instacado <github@instacado.com>
2014-07-21 10:25:17 +1000

    WIP
    Update existing posts in ASX Market Update category (4825)
    Currently doesn't work.

---
+++ MoneyMorning/asx_update.php
@@ -0,0 +1,28 @@
+<?php
+function andy_addtocontent($andy_postarray) {
+    $postcount = count($andy_postarray);
+    $andy_posturl = get_permalink($andy_postarray[0]);
+    for ($i=1; $i < $postcount; $i++) { 
+        $content = get_post_field('post_content', $andy_postarray[$i]);
+        $content = '<strong>UPDATE:</strong> Newer information can be found on this stock <a href="'.$andy_posturl.'">here</a><br/>'.$content;
+        return $content;
+    }
+}
+function andy_fillpostarray($andy_asxcode) {
+    $args = array('category' => '7', 'meta_key' => 'asx_code', 'meta_value' => $andy_asxcode);
+    $andy_loadedcategory = get_posts($args);
+    $andy_loadedcount = count($andy_loadedcategory);
+    for ($i=0; $i < $andy_loadedcount; $i++) { 
+        $andy_postarray[$i] = $andy_loadedcategory->ID;
+    }
+    return $andy_postarray;
+}
+function andy_postaction($new_status, $old_status, $post) {
+    $andy_asxcode = get_post_meta($post->ID, 'asx_code', true);
+    if ($andy_asxcode && $new_status = 'publish') {
+        andy_fillpostarray($andy_asxcode);
+        andy_addtocontent($andy_postarray);
+    }
+}
+add_action('transition_post_status', 'andy_postaction', 10, 3);
+?>
